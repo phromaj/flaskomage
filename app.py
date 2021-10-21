02f5746c96a954b34474b495cc82c6f8263da90e
John Doe <lenapancher@gmail.com>
2021-10-21 11:22:39 +0200

    git rebase resolved conflict

--- app.py
+++ app.py
@@ -1,18 +1,125 @@
-from flask import Flask
+from flask import Flask, request
 import pymongo
-from pymongo import collection
 
 app = Flask(__name__)
 
-username = 'lucas'
-password = 'gauvain'
+username = 'lena'
+password = 'admin'
 client = pymongo.MongoClient(
-    f"mongodb+srv://{username}:{password}@cluster0.qfqkw.mongodb.net/?retryWrites=true&w=majority"
+    f"mongodb+srv://{username}:{password}@coding.mvpr0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 )
 db = client.flaskomage
 fromages_coll = db.fromages
 regions_coll = db.regions
 
-@app.route('/users', methods=['POST', 'GET'])
-def gangngegwe():
-    return f"bondou"
+
+# @app.route('/get', methods=['GET'])
+# def get():
+#     get_value = fromages_coll.find().count()
+#     print(get_value)
+#     return f"Get {get_value}"
+
+
+@app.route('/insert_one', methods=['POST'])
+def insert_one():
+    """
+    Function to insert a document in the cheese collection.
+    :return: 
+    """
+    req_data = request.get_json()
+    new_fromage = fromages_coll.insert_one(req_data).inserted_id
+    print(req_data)
+    return f"Inserted {new_fromage}"
+
+
+# Test in Postman
+#     {
+#     "nom": "insert_one",
+#     "departement": ["insert_one"],
+#     "pate": "insert_one",
+#     "lait": "insert_one",
+#     "annee_aoc": "insert_one",
+#     "fromage_id": 0
+#     }
+
+
+@app.route('/insert_many', methods=['POST'])
+def insert_many():
+    """
+    Function that allows to insert many documents in the cheese collection.
+    :return:
+    """
+    req_data = request.get_json()
+    new_fromages = fromages_coll.insert_many(req_data).inserted_ids
+    print(req_data)
+    return f"Inserted {new_fromages}"
+
+
+# Test in Postman
+# [
+#     {
+#     "nom": "insert_many",
+#     "departement": ["insert_many"],
+#     "pate": "insert_many",
+#     "lait": "insert_many",
+#     "annee_aoc": "insert_many",
+#     "fromage_id": 0
+#     },{
+#      "nom": "insert_many",
+#      "departement": ["insert_many"],
+#      "pate": "insert_many",
+#      "lait": "insert_many",
+#      "annee_aoc": "insert_many",
+#      "fromage_id": 0
+#      },{
+#      "nom": "insert_many",
+#      "departement": ["insert_many"],
+#      "pate": "insert_many",
+#      "lait": "insert_many",
+#      "annee_aoc": "insert_many",
+#      "fromage_id": 0
+#      }
+# ]
+
+@app.route('/delete_one/<id_fromage>', methods=['DELETE'])
+def delete_one(id_fromage):
+    """
+    Function to delete a cheese designated by its id.
+    :return:
+    """
+    fromages_coll.delete_one({
+        "fromage_id": int(id_fromage)
+    })
+    return "Delete"
+
+
+@app.route('/delete_many', methods=['DELETE'])
+def delete_many():
+    """
+    Function to delete all the documents in the cheese collection.
+    :return:
+    """
+    fromages_coll.delete_many({})
+    return "Delete"
+
+
+@app.route('/update_one/<id_fromage>', methods=['PUT'])
+def update_one(id_fromage):
+    """
+    Function to update a cheese designated by its id.
+    :return:
+    """
+    filter_by_id = {'fromage_id': int(id_fromage)}
+    new_values = {"$set": request.get_json()}
+    fromages_coll.update_one(filter_by_id, new_values)
+    return "Update"
+
+# Test in Postman
+#     {
+#     "nom": "update_one",
+#     "departement": ["update_one"],
+#     "pate": "update_one",
+#     "lait": "update_one",
+#     "annee_aoc": "update_one",
+#     "fromage_id": 0
+#     }
--- db_generator.py
+++ db_generator.py
@@ -1,22 +1,23 @@
+from flask import Flask
 from bs4 import BeautifulSoup
 import pymongo
 import requests
 import os
 
+app = Flask(__name__)
 
-def main():
-    username = 'lucas'
-    password = 'gauvain'
-    client = pymongo.MongoClient(
-        f"mongodb+srv://{username}:{password}@cluster0.qfqkw.mongodb.net/?retryWrites=true&w=majority"
-    )
-    db = client.flaskomage
-    fromages_coll = db.fromages
-    regions_coll = db.regions
 
-    scrape_regions()
-    scrape_fromages()
+username = 'lucas'
+password = 'gauvain'
+client = pymongo.MongoClient(
+    f"mongodb+srv://{username}:{password}@cluster0.qfqkw.mongodb.net/?retryWrites=true&w=majority"
+)
+db = client.flaskomage
+fromages_coll = db.fromages
+regions_coll = db.regions
 
+
+@app.route('/scrape_regions', methods=['POST'])
 def scrape_regions():
     wiki = "https://fr.wikipedia.org/wiki/R%C3%A9gion_fran%C3%A7aise"
     header = {
@@ -118,15 +119,18 @@ def scrape_regions():
         regions_to_send.append(regions)
 
     print(regions_to_send)
+    insert_value = fromages_coll.insert_many(regions_to_send).inserted_ids
+    return str(insert_value)
 
 
+@app.route('/scrape_fromages', methods=['POST'])
 def scrape_fromages():
     wiki = "https://fr.wikipedia.org/wiki/Liste_des_AOC_et_AOP_laiti%C3%A8res_fran%C3%A7aises"
     header = {
         'User-Agent': 'Mozilla/5.0'
     }
     page = requests.get(wiki, headers=header)
-    soup = BeautifulSoup(page.content)
+    soup = BeautifulSoup(page.content, features="html.parser")
 
     tables = soup.findAll("table", {"class": "wikitable"})
     
@@ -192,8 +196,8 @@ def scrape_fromages():
     fromages_to_send = []
     count = 0
     for t in tableau:
-        
-        count = count+1
+
+        count = count + 1
 
         final_result = []
 
@@ -210,7 +214,7 @@ def scrape_fromages():
             final_result = d.split(' ')
             final_result[:] = [x for x in final_result if x]
             for i in range(len(final_result)):
-                if  "--" in final_result[i]:
+                if "--" in final_result[i]:
                     final_result[i] = final_result[i].replace("--", "-et-")
         if is_list:
             splitted_deps = t[3].split('(')
@@ -221,7 +225,7 @@ def scrape_fromages():
             final_result = formatted_deps.split(' ')
             final_result[:] = [x for x in final_result if x]
             for i in range(len(final_result)):
-                if  "--" in final_result[i]:
+                if "--" in final_result[i]:
                     final_result[i] = final_result[i].replace("--", "-et-")
 
         fromages = {
@@ -235,7 +239,5 @@ def scrape_fromages():
         fromages_to_send.append(fromages)
 
     print(fromages_to_send)
-
-
-if __name__ == '__main__':
-    main()
+    insert_value = fromages_coll.insert_many(fromages_to_send).inserted_ids
+    return str(insert_value)
